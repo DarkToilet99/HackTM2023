3c1b53d2949a8cc548bd50d5ee7999fb03f2fca7
DarkToilet99 <ane.mac99@gmail.com>
2023-05-13 00:02:25 +0300

    Make search component mock

--- recipedex_ui/src/app/app.module.ts
+++ recipedex_ui/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { GoogleMapsModule } from '@angular/google-maps'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeSearchComponent } from './recipe-search/recipe-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeSearchComponent
   ],
   imports: [
     BrowserModule,
