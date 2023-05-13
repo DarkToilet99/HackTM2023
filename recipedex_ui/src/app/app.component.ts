377098890951ef83614f4d800d6873b6e8db2dcf
DarkToilet99 <ane.mac99@gmail.com>
2023-05-13 04:22:31 +0300

    Added circle on the map

--- recipedex_ui/src/app/app.component.ts
+++ recipedex_ui/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { Loader } from "@googlemaps/js-api-loader"
 import { environment } from 'src/environments/environment.development';
 
@@ -10,21 +10,83 @@ import { environment } from 'src/environments/environment.development';
 export class AppComponent implements OnInit {
   
   map: google.maps.Map | undefined;
+  
+  latitude: number = 0;
+  longitude: number = 0;
+  zoom: number = 0;
+
+  center: google.maps.LatLngLiteral = {} as google.maps.LatLngLiteral;
+  currentLocationMarker : google.maps.Marker | undefined;
+
+  circle: google.maps.Circle = {} as google.maps.Circle;
+  radius = 5000;
+  
+  address: string = "";
+  private geoCoder : any;
   loader = new Loader({
     apiKey: environment.mapApiKey,
     version: "weekly",
   });
   
   
+  constructor(){
+  }
+  
   ngOnInit() {
-    
     this.loader.load().then(async () => {
-      const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
-      this.map = new Map(document.getElementById("map") as HTMLElement, {
-        center: { lat: 45.7552446, lng: 21.2127462 },
-        zoom: 13,
-      });
+      this.geoCoder = new google.maps.Geocoder;
+      this.setCurrentLocation();
     });
+  }
+
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.center = { lat: this.latitude, lng: this.longitude }
+        this.zoom = 8;
+        this.getAddress(this.latitude, this.longitude);
+        
+      });
+    }
+  }
+
+  getAddress(latitude : any, longitude : any) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, async (results : any, status : any) => {
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 13;
+          this.address = results[0].formatted_address;
+          const { Map } = await google.maps.importLibrary("maps") as google.maps.MapsLibrary;
+          this.map = new Map(document.getElementById("map") as HTMLElement, {
+            center: this.center,
+            zoom: this.zoom,
+          });
+          this.currentLocationMarker = new google.maps.Marker({
+            position: new google.maps.LatLng(this.latitude, this.longitude),
+            map: this.map!,
+            title: 'current location'
+        });
+
+        this.drawPolygon();
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+    
+    });
+  }
+
+  async drawPolygon(){
+    this.circle = new google.maps.Circle({
+      center: this.center,
+      radius: this.radius,
+    })
+
+    this.circle.setMap(this.map!)
   }
 
 }
